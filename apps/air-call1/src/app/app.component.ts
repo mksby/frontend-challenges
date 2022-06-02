28dc9cbda13a1e99f4541dd1075217414528e7f2
Maksim Troitski <maxim.troitsky@gmail.com>
2022-06-02 23:07:37 +0300

    initial commit

---
+++ apps/air-call1/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'frontend-challenges-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'air-call1';
+}
